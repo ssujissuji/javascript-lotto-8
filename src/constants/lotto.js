cb0edacbaed26901dcc3125b10ed35b517e7c86e
Suji <soojiya2003@gmail.com>
2025-11-03 22:44:15 +0900

    refactor(constants): 로또 상수값(가격, 범위, 개수 등) 정의

---
+++ src/constants/lotto.js
@@ -0,0 +1,12 @@
+export const LOTTO_NUMBER_COUNT = 6;
+export const LOTTO_PRICE = 1000;
+export const LOTTO_NUMBER_MIN = 1;
+export const LOTTO_NUMBER_MAX = 45;
+
+export const PRIZE_TABLE = {
+  3: 5000,
+  4: 50000,
+  5: 1500000,
+  5.5: 30000000,
+  6: 2000000000,
+};
