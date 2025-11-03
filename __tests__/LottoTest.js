74b42eabd911468a95a99fb8b21f7b87592be4df
Suji <soojiya2003@gmail.com>
2025-11-03 22:43:23 +0900

    test(lotto): 로또 클래스 검증 테스트 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,18 +1,71 @@
-import Lotto from "../src/Lotto";
+import Lotto from '../src/Lotto';
 
-describe("로또 클래스 테스트", () => {
-  test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
+describe('로또 클래스 테스트', () => {
+  test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow("[ERROR]");
+    }).toThrow('[ERROR]');
   });
 
   // TODO: 테스트가 통과하도록 프로덕션 코드 구현
-  test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
+  test('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow("[ERROR]");
+    }).toThrow('[ERROR]');
+  });
+
+  test('로또 번호의 개수가 6개보다 적으면 예외가 발생한다.', () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5]);
+    }).toThrow('[ERROR]');
+  });
+
+  test('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, 5]);
+    }).toThrow('[ERROR]');
+  });
+
+  test('로또 번호에 숫자가 아닌 값이 있으면 예외가 발생한다.', () => {
+    expect(() => {
+      new Lotto([1, 2, 'a', 4, 5, 6]);
+    }).toThrow('[ERROR]');
+  });
+
+  test('로또 번호가 1 미만이거나 45 초과인 숫자가 있으면 예외가 발생한다.', () => {
+    expect(() => {
+      new Lotto([0, 2, 3, 4, 5, 6]);
+    }).toThrow('[ERROR]');
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, 46]);
+    }).toThrow('[ERROR]');
+  });
+
+  test('로또 번호에 음수나 소수가 포함되면 예외가 발생한다.', () => {
+    expect(() => {
+      new Lotto([-1, 2, 3, 4, 5, 6]);
+    }).toThrow('[ERROR]');
+    expect(() => {
+      new Lotto([1.5, 2, 3, 4, 5, 6]);
+    }).toThrow('[ERROR]');
   });
 
   // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+
+  test('유효한 6개의 번호로 생성된 경우 예외가 발생하지 않는다.', () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, 6]);
+    }).not.toThrow();
+  });
+
+  test('로또 번호는 오름차순으로 정렬되어 저장된다.', () => {
+    const lotto = new Lotto([6, 1, 3, 5, 2, 4]);
+    expect(lotto.getNumbers()).toEqual([1, 2, 3, 4, 5, 6]);
+  });
+
+  test('당첨 번호와 일치하는 개수를 반환한다.', () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    const winningNumbers = [1, 3, 5, 7, 9, 11];
+    expect(lotto.countMatchingNumbers(winningNumbers)).toBe(3);
+  });
 });
