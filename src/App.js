5ddaee425ed99fae3b492840f5a81020bffc4092
Suji <soojiya2003@gmail.com>
2025-11-03 22:43:35 +0900

    feat(app): 전체 게임 실행 흐름(App.run) 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import LottoGame from './LottoGame.js';
+import InputView from './views/InputView.js';
+import OutputView from './views/OutputView.js';
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const money = await InputView.readPurchaseAmount();
+      const game = new LottoGame(money);
+
+      OutputView.printLottoTickets(game.getTickets());
+
+      const winningNumbers = await InputView.readWinningNumbers();
+      const bonuseNumber = await InputView.readBonusNumber();
+
+      const results = game.checkResult(winningNumbers, bonuseNumber);
+      const profitRate = game.calculateProfitRate();
+
+      OutputView.printStatistics(results, profitRate);
+    } catch (error) {
+      OutputView.printError(error.message);
+    }
+  }
 }
 
 export default App;
